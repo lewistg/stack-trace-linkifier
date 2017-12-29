e7354f25ecf564868deca841d18a72d2c7fcc5e5
Ty Lewis <ty@lucidchart.com>
2017-12-28 20:45:22 -0700

    Only linkify the source location string

--- content/linkinsert.js
+++ content/linkinsert.js
@@ -1,29 +1,34 @@
-import * as util from './util.js';
-
-const stackFramePattern = /at.*\(((http|https):\/\/.*):(\d+):\d+\)/g;
-const linkClass = '_λ_stack_trace_link_λ_';
+import * as Util from './util.js';
 
 export function insertLinks(rootNodes, openSourceFile) {
-    const childQueue = rootNodes || [];
-    while (childQueue.length > 0) {
-        let c = childQueue.pop();
-        childQueue.push(...(c.childNodes || []));
-        visit(c, openSourceFile);
-    }
+    setTimeout(function() {
+        const childQueue = rootNodes || [];
+        while (childQueue.length > 0) {
+            let c = childQueue.pop();
+            childQueue.push(...(c.childNodes || []));
+            visit(c, openSourceFile);
+        }
+    }, 1000);
 }
 function visit(node, openSourceFile) {
-    if (!(node instanceof Text) || !stackFramePattern.test(node.data)) {
+    if (!(node instanceof Text) || !StackFramePatterns.stackFrame.test(node.data)) {
         return;
     }
+    const linkifiedStackTraceNodes = getLinkifiedStackTrace(node.data);
+    Util.replaceNodeWithNodes(node, linkifiedStackTraceNodes);
+    addLinkListeners(linkifiedStackTraceNodes, openSourceFile);
+}
 
-    let dataWithLinks = util.htmlEscape(node.data);
-    util.resetRegExp(stackFramePattern);
-    dataWithLinks = dataWithLinks.replace(stackFramePattern, `<span class="${linkClass}" style="text-decoration: underline; cursor: pointer">$&</span>`);
-
-    const nodes = util.parseHTMLString(dataWithLinks);
-    util.replaceNodeWithNodes(node, nodes);
-
-    addLinkListeners(nodes, openSourceFile);
+function getLinkifiedStackTrace(stackTraceString) {
+    let htmlString = Util.htmlEscape(stackTraceString);
+    Util.resetRegExp(StackFramePatterns.stackFrame);
+    htmlString = htmlString.replace(StackFramePatterns.stackFrame, function(stackFrameString) {
+        return stackFrameString.replace(
+            StackFramePatterns.sourceLocation, 
+            `<span class="${linkClass}" style="text-decoration: underline; cursor: pointer">$&</span>`
+        );
+    });
+    return Util.parseHTMLString(htmlString);
 }
 
 function addLinkListeners(nodes, openSourceFile) {
@@ -39,20 +44,37 @@ function addLinkListeners(nodes, openSourceFile) {
 function onMousedownLink(event, openSourceFile) {
     event.preventDefault();
     event.stopPropagation();
-
-    util.resetRegExp(stackFramePattern);
-    const match = stackFramePattern.exec(event.target.innerText);
-    if(!match) {
+    const sourceLocation = getSourceLocation(event.target.innerText);
+    if (!sourceLocation) {
         return;
     }
-    const sourceUrl = match[1];
-    const lineNumber = +match[3] - 1;
+    const [sourceUrl, lineNumber] = sourceLocation;
     openSourceFile(sourceUrl, lineNumber);
+}
+
+function getSourceLocation(sourceLocationString) {
+    const match = StackFramePatterns.sourceLocation.exec(sourceLocationString);
+    if(!!match) {
+        return [
+            match[StackFramePatterns.sourceLocationGroupIndices.url],
+            +match[StackFramePatterns.sourceLocationGroupIndices.lineNumber] - 1
+        ];
+    }
 }
 
 export function removeLinks() {
     const links = document.querySelectorAll(`.${linkClass}`);
     links.forEach(function(link) {
-        util.replaceNodeWithNodes(link, link.childNodes);
+        Util.replaceNodeWithNodes(link, link.childNodes);
     });
 }
+
+const linkClass = '_λ_stack_trace_link_λ_';
+const StackFramePatterns = {
+    sourceLocation: /((https?:\/)?\/.*):(\d+):\d+/,
+    sourceLocationGroupIndices: {
+        url: 1,
+        lineNumber: 3
+    }
+}
+StackFramePatterns.stackFrame = new RegExp(`at.*\(${StackFramePatterns.sourceLocation.source}\)`, 'g');
