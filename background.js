3eccfe3788f6247c4d766a7e24e38b8e83df626c
Ty Lewis <ty@lucidchart.com>
2017-12-30 12:18:57 -0700

    Factor the background script functionality into a class also reload content script on refresh

--- background.js
+++ background.js
@@ -2,30 +2,73 @@ chrome.runtime.onConnect.addListener(function(devToolsPort) {
     if (devToolsPort.name != 'devtools-page') {
         return;
     }
-    let tabId;
-    let contentScriptPort;
-    function devToolsListener(message, sender, sendResponse) {
-        tabId = message.inspectedTabId;
-        chrome.tabs.executeScript(
-            message.inspectedTabId, 
-            {file: './content_script.js'},
-            onContentScriptExecuted
-        );
-    }
-    function onContentScriptExecuted() {
-        contentScriptPort = chrome.tabs.connect(tabId);
-        contentScriptPort.onMessage.addListener(contentScriptListener);
-    }
-    function contentScriptListener(message, sender, sendResponse) {
-        devToolsPort.postMessage(message);
-    }
-    devToolsPort.onMessage.addListener(devToolsListener);
-    devToolsPort.onDisconnect.addListener(function listener() {
-        contentScriptPort.disconnect();
-
-        devToolsPort.onMessage.addListener(devToolsListener);
-        contentScriptPort.onMessage.removeListener(contentScriptListener);
-
-        devToolsPort.onDisconnect.removeListener(listener);
-    });
+    new ContentScriptDevToolsPipe(devToolsPort);
 });
+
+/**
+ * Message pipe between the content script and the dev tools script.
+ * @param {Port} devToolsPort
+ * @constructor
+ */
+function ContentScriptDevToolsPipe(devToolsPort) {
+    /** @private {Port} */
+    this.devToolsPort = devToolsPort;
+    /** @private {Port} */
+    this.contentScriptPort;
+    /** @private {number} */
+    this.inspectedTabId;
+
+    this.devToolsPort.onMessage.addListener((message, sender, sendResponse) => {
+        if (!message.inspectedTabId) {
+            return;
+        }
+        this.inspectedTabId = message.inspectedTabId;
+        this.loadContentScript();
+    });
+    this.devToolsPort.onDisconnect.addListener((message, sender, sendResponse) => this.onDevToolsClosed());
+    this.handleTabRefresh();
+
+}
+
+/**
+ * @private
+ */
+ContentScriptDevToolsPipe.prototype.loadContentScript = function() {
+    chrome.tabs.executeScript(
+        this.inspectedTabId,
+        {file: './content_script.js'},
+        () => {
+            this.contentScriptPort = chrome.tabs.connect(this.inspectedTabId);
+            this.contentScriptPort.onMessage.addListener((message, sender, sendResponse) => this.pipeMessageToDevTools(message));
+        }
+    );
+}
+
+/**
+ * @private
+ */
+ContentScriptDevToolsPipe.prototype.onDevToolsClosed = function() {
+    this.contentScriptPort.disconnect();
+}
+
+/**
+ * @private
+ */
+ContentScriptDevToolsPipe.prototype.handleTabRefresh = function() {
+    chrome.webNavigation.onDOMContentLoaded.addListener((details) => {
+        if (details.tabId == this.inspectedTabId) {
+            this.loadContentScript();
+        }
+
+    },{url: [
+        {hostEquals: window.location.host}, 
+        {pathEquals: window.location.pathName},
+    ]});
+}
+
+/**
+ * @private
+ */
+ContentScriptDevToolsPipe.prototype.pipeMessageToDevTools = function(message) {
+    this.devToolsPort.postMessage(message);
+}
